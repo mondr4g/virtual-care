703e3bf87c4a95fc56163ebac0aa775d59ddde7e
Eduardo Mena <46607259+mondr4g@users.noreply.github.com>
2021-06-15 01:18:45 -0500

    Formulario de registro de doctor

--- v-care/src/app/app-routing.module.ts
+++ v-care/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegistComponent } from './regist/regist.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistComponent }
 ];
 
 @NgModule({
